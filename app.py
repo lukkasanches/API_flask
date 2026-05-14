a85f8825f3a227525caf32bf45437989cb1a6e81
lukkasanches <lukadorock2008@gmail.com>
2026-05-14 13:39:00 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 from flask import flask
 from flask import jsonify
 from flask_cors import CORS 
-app = Flask(__name__)
 
+app = Flask(__name__)
 CORS (app) 
 
-@app.route("/")
-def inicio():
+usuarios = [
+    {
+        "nome": "Luka Sanches",
+        "email": "luka@gmail.com",
+        "telefone": "(14) 99999-0002"
+    },
 
-    return "API funcionando!"
+    {
+        "nome": "Thiago Soares",
+        "email": "thiago@gmail.com",
+        "telefone": "(14) 99999-0001"
+    },
 
-app.run(debug=True)
+    {
+        "nome": "Maria Silva",
+        "email": "maria@gmail.com",
+        "telefone": "(14) 99999-0000"
+    }
+]
+
+
+@app.route('/usuarios', methods=['GET'])
+def listar_usuarios():
+    return jsonify(usuarios)
+
+if __name__ == '__main__':
+    app.run(debug=True)
